0a5f31d41420fb593b09ca60206f57023babf06f
apollonfq <38276802+apollonfq@users.noreply.github.com>
2020-05-28 14:56:55 +0800

    first commit for CI/CD testing

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "This is CI/CD-AWS EKS testing for SYT"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
